13de4974a04ec3d4bb4e40c801bd22c0b3095405
Ashish Joshi <ashishjoshi2023@gmail.com>
2023-03-16 22:22:39 +0530

    reverse a string with only one variable

--- recursion/ReverseAString.cpp
+++ recursion/ReverseAString.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-void reverse(string &s,int i,int j){
-  if(i>j) 
+void reverse(string &s,int i,int n){
+  if(i>(n-1)/2) 
   return ;
-  swap(s[i],s[j]);
-  reverse(s,i+1,j-1);
+  swap(s[i],s[n-i-1]);
+  reverse(s,i+1,n);
 }
+// void reverse(string &s,int i,int j){
+//   if(i>j) 
+//   return ;
+//   swap(s[i],s[j]);
+//   reverse(s,i+1,j-1);
+// }
 int main() {
   string s;
   cin>>s;
-  reverse(s,0,s.size()-1);
+  reverse(s,0,s.size());
   cout<<s;
   return 0;
 }
