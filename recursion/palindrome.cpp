7a84f6de82c18106b366976fc644e9e06dae7446
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-03-16 17:08:38 +0000

    palindrome

--- recursion/palindrome.cpp
+++ recursion/palindrome.cpp
@@ -1,7 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    
-    return 0;
+bool check(string &s,int i,int j){
+  if(i>j) 
+  return true;
+  if(s[i]!=s[j]) return false;
+  return check(s,i+1,j-1);
+}
+int main() {
+  string s;
+  cin>>s;
+  cout<<check(s,0,s.size()-1);
+  return 0;
 }
