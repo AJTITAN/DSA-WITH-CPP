05d363ab3098aa0ad52c5d56a6ed19eeffdb4990
Ashish Joshi <ashishjoshi2023@gmail.com>
2023-04-02 15:05:33 +0530

    circular queue

--- queues/circular_queue.cpp
+++ queues/circular_queue.cpp
@@ -14,30 +14,22 @@ class queue{
         rear = -1;
     }
     void push(int data){
-        if((front ==0 && rear==size-1) || (rear == front-1)) return ;
-        if(front==-1) {
-            front=0;
-            rear=0; 
+        if((front==0 && rear==size-1) || (rear==(front-1)%(size-1))) return ;
+        if(front==-1){
+            front =0;
+            rear = 0;
         }
         else if(rear==size-1 && front!=0){
-            rear=0;
+            rear = 0;
         }
-        else{
-            rear++;
-        }
-        arr[rear]= data;
+        else rear++;
+        arr[rear] = data;
     }
     void pop(){
-        if(front==-1){
-            return ;
-        }
-         if(front==rear){
-            front = rear = -1;
-        }
-            else if(front==size-1)front = 0;
-        else{
-            front++;
-        }
+        if(front==-1) return ;
+        if(front==rear) front = rear = -1;
+        else if(front==size-1) front=0;
+        else front++;
     }
     void disp(){
         for(int i=front;i<=rear;i++){
@@ -57,7 +49,6 @@ int main()
     q.disp();
     q.pop();
     q.pop();
-    q.disp();
     q.push(5);
     q.disp();
     return 0;
